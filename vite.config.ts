25f123868a10bf36fa985e02c04c93d45eace77e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-12-29 02:28:02 +0000

    fix - corrected base path in Vite configuration (#104)
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 const viteConfig = defineViteConfig({
   plugins: [react()],
-  base: '/ui/',
+  base: '/ui/sports-agenda/',
 });
 
 const vitestConfig = defineVitestConfig({
