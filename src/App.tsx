75dd8603d9e0586dfe1d75356f2440fa2a677ead
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-12-29 02:08:43 +0000

    feat - added initial project structure and modified app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+/// <reference types="vite-plugin-svgr/client" />
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="assets/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
