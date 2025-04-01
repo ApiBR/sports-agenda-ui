a8ae26eda71863fd4d77edeafbb0793ca7b1d19b
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-04-01 01:19:18 +0100

    feat - add base directory configuration for new layout

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ui/sports-agenda/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
